062eadb7b0ee1138b862c374482366675560d583
santatsut <philarthuang@gmail.com>
2025-09-25 12:00:15 +0200

    start

---
+++ index.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Document</title>
+</head>
+<body>
+    <div id="navbar">
+        <h1>Poäng Jakt</h1>
+        <p>Detta är Poängen just nu</p>
+    </div>
+    <div id="content">
+        <div id="leftSide">
+            <h2>Teknik</h2>
+            <h1>100</h1>
+        </div>
+        <div id="rightSide">
+            <h2>Estet</h2>
+            <h1>99</h1>
+        </div>
+        <div id="bottomSide">
+            <h1>Lista på Uppgifter</h1>
+            <h2>bla bla</h2>
+            <h2 style="text-decoration: line-through;">Slå en etta med en gurka</h2>
+            <h2 style="text-decoration: line-through;">5 armhävningar under lektion</h2>
+            <h2 style="text-decoration: line-through;">äta en hel tårta</h2>
+
+        </div>
+    </div>
+</body>
+</html>
